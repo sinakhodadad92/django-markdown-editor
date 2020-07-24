ea946c826be52c8724b3b3a604f39897c80cb464
sinakhodadad92 <sinakhodadad92@gmail.com>
2020-07-24 18:24:34 +0200

    flake8 fixing

--- martor_demo/app/test.py
+++ martor_demo/app/test.py
@@ -6,8 +6,7 @@ from app.models import Post
 
 class PostTestCase(TestCase):
     def testPost(self):
-        post = Post(title = "My Title", description = "Blurb", wiki = "Post Body")
+        post = Post(title="My Title", description="Blurb", wiki="Post Body")
         self.assertEqual(post.title, "My Title")
         self.assertEqual(post.description, "Blurb")
         self.assertEqual(post.wiki, "Post Body")
-
